de26b8f95009a77fad887594b314b0f77dac5071
Oshioke-Salaki <salaki1902@gmail.com>
2023-06-30 10:35:12 +0100

    first commit

---
+++ src/Accordion.jsx
@@ -0,0 +1,12 @@
+import Item from "./Item";
+function Accordion({ faqs }) {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, i) => (
+        <Item num={i + 1} key={i} title={faq.title} text={faq.text} />
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
---
+++ src/Item.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Item({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className={isOpen ? "item open" : "item"}
+      onClick={() => setIsOpen((cur) => !cur)}
+    >
+      <p className="number">{num < 9 ? `0${num}` : num}</p>
+      <h1 className={isOpen ? "text title" : "title"}>{title}</h1>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
+
+export default Item;
