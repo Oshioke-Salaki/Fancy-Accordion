87fae256bb1eaec0b80207c214609f34cb2848f8
Oshioke-Salaki <salaki1902@gmail.com>
2023-06-30 16:44:03 +0100

    updated

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,8 +3,8 @@
 function Item({ num, title, text, isOpen, setOpen }) {
   return (
     <div
-      className={isOpen ? "item open" : "item"}
-      onClick={() => setOpen(num - 1)}
+      className={isOpen === num - 1 ? "item open" : "item"}
+      onClick={() => (isOpen === num - 1 ? setOpen(null) : setOpen(num - 1))}
     >
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <h1 className="title">{title}</h1>
