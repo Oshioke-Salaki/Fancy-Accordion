5b95452ca0f4b2c2dbe5e5f7a2f4b7497ad5d72e
Oshioke-Salaki <salaki1902@gmail.com>
2023-06-30 11:15:51 +0100

    updated

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,10 +1,37 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Item from "./Item";
-function Accordion({ faqs }) {
+function Accordion() {
+  const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    },
+    {
+      title: "How long do I have to return my chair?",
+      text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    },
+    {
+      title: "Do you ship to countries outside the EU?",
+      text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+    },
+  ];
+  const [isOpen, setIsOpen] = useState(null);
+
+  function setOpen(num) {
+    setIsOpen(num);
+  }
   return (
     <div className="accordion">
       {faqs.map((faq, i) => (
-        <Item num={i + 1} key={i} title={faq.title} text={faq.text} />
+        <Item
+          num={i + 1}
+          key={i}
+          title={faq.title}
+          text={faq.text}
+          setOpen={setOpen}
+          isOpen={isOpen}
+        />
       ))}
     </div>
   );
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 
-function Item({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function Item({ num, title, text, isOpen, setOpen }) {
   return (
     <div
       className={isOpen ? "item open" : "item"}
-      onClick={() => setIsOpen((cur) => !cur)}
+      onClick={() => setOpen(num - 1)}
     >
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <h1 className="title">{title}</h1>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
+      <p className="icon">{isOpen === num - 1 ? "-" : "+"}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen === num - 1 && <div className="content-box">{text}</div>}
     </div>
   );
 }
